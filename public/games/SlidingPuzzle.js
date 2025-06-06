c0dada201d70d60574afb2d702e7d7cea31b9308
Nishchal Gupta <nishchalgupta1907@gmail.com>
2025-06-06 17:01:44 +0530

    updated sliding puzzle

--- public/games/SlidingPuzzle.js
+++ public/games/SlidingPuzzle.js
@@ -71,9 +71,10 @@ class GameScene extends Phaser.Scene {
         for (const key in _CONFIG.soundsLoader) {
             this.load.audio(key, [_CONFIG.soundsLoader[key]]);
         }
+        for (const key in _CONFIG.libLoader) {
+            this.load.image(key, _CONFIG.libLoader[key]);
+        }
 
-
-        this.load.image("pauseButton", "https://aicade-ui-assets.s3.amazonaws.com/GameAssets/icons/pause.png");
         this.load.bitmapFont('pixelfont',
             'https://aicade-ui-assets.s3.amazonaws.com/GameAssets/fonts/pix.png',
             'https://aicade-ui-assets.s3.amazonaws.com/GameAssets/fonts/pix.xml');
