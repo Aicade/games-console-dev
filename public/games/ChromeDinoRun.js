027ea478504a5b1cbbafabac5112ccb86f10d36f
Nishchal Gupta <nishchalgupta1907@gmail.com>
2025-06-06 11:56:40 +0530

    made changes to chromedino

--- public/games/ChromeDinoRun.js
+++ public/games/ChromeDinoRun.js
@@ -17,12 +17,14 @@ class GameScene extends Phaser.Scene {
         for (const key in _CONFIG.soundsLoader) {
             this.load.audio(key, [_CONFIG.soundsLoader[key]]);
         }
+        for (const key in _CONFIG.libLoader) {
+            this.load.image(key, _CONFIG.libLoader[key]);
+        }
 
-        this.load.image('heart', 'https://aicade-ui-assets.s3.amazonaws.com/GameAssets/icons/heart.png');
         this.load.bitmapFont('pixelfont',
             'https://aicade-ui-assets.s3.amazonaws.com/GameAssets/fonts/pix.png',
             'https://aicade-ui-assets.s3.amazonaws.com/GameAssets/fonts/pix.xml');
-        this.load.image("pauseButton", "https://aicade-ui-assets.s3.amazonaws.com/GameAssets/icons/pause.png");
+
 
         addEventListenersPhaser.bind(this)();
         displayProgressLoader.call(this);
