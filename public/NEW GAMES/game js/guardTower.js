f9f15a01c9168619c3e64d7ae31a500d4d1d9ef1
Nishchal Gupta <nishchalgupta1907@gmail.com>
2025-06-08 00:23:43 +0530

    updated guard tower

--- public/NEW GAMES/game js/guardTower.js
+++ public/NEW GAMES/game js/guardTower.js
@@ -2,10 +2,11 @@
 class GameScene extends Phaser.Scene {
     constructor() {
         super({ key: 'GameScene' });
+         this.isGameOver = false;
     }
 
     preload() {
-        this.isGameOver = false;
+       
 
         addEventListenersPhaser.bind(this)();
 
