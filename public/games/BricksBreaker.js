6ce10632aac4754d505ac8393cab0056a1e4a119
Nishchal Gupta <nishchalgupta1907@gmail.com>
2025-06-09 01:45:20 +0530

    updated chess.json

--- public/games/BricksBreaker.js
+++ public/games/BricksBreaker.js
@@ -5,6 +5,8 @@ _CONFIG.deviceOrientation = isMobile ? "portrait" : "landscape";
 class GameScene extends Phaser.Scene {
   constructor() {
     super({ key: "GameScene" });
+    this.score = 0; // Initialize score
+    this.isGameOver = false; // Initialize game over state
   }
 
   preload() {
@@ -28,7 +30,6 @@ class GameScene extends Phaser.Scene {
   }
 
   create() {
-    this.score = 0;
     this.width = this.game.config.width;
     this.height = this.game.config.height;
 
@@ -58,7 +59,6 @@ class GameScene extends Phaser.Scene {
     this.pauseButton.setScale(2).setScrollFactor(0).setDepth(11);
     this.pauseButton.on("pointerdown", () => this.pauseGame());
     this.vfx = new VFXLibrary(this);
-    gameSceneCreate(this);
 
     this.sounds = {};
     for (const key in _CONFIG.soundsLoader) {
@@ -68,10 +68,155 @@ class GameScene extends Phaser.Scene {
     // Set background music volume to 25% and enable looping
     this.sounds.background.setVolume(0.25).setLoop(true).play();
     this.input.keyboard.disableGlobalCapture();
+
+    // Call the method to set up game objects (replacing gameSceneCreate)
+    this.setupGame();
+  }
+
+  // Moved gameSceneCreate logic into a class method
+  setupGame() {
+    this.physics.world.setBoundsCollision(1, 1, 1, 0);
+
+    this.bricks = this.physics.add.group({
+      immovable: true,
+      allowGravity: false,
+    });
+
+    // Set brick formation settings based on device type:
+    // For mobile devices: 13 columns and 5 rows
+    // For PC devices: 15 columns and 4 rows
+    const numBricksX = isMobile ? 13 : 15;
+    const numBricksY = isMobile ? 5 : 4;
+    const brickSpacingX = 50;
+    const brickSpacingY = 52;
+    const brickWidth = 64;
+    const brickHeight = 64;
+
+    // Calculate total width of the brick formation and starting X coordinate to center it
+    const formationWidth = brickSpacingX * (numBricksX - 1) + brickWidth;
+    const startX = (this.width - formationWidth) / 2 + brickWidth / 2;
+    const startY = 100;
+
+    for (let y = 0; y < numBricksY; y++) {
+      for (let x = 0; x < numBricksX; x++) {
+        const brickX = startX + x * brickSpacingX;
+        const brickY = startY + y * brickSpacingY;
+        const brick = this.bricks
+          .create(brickX, brickY, "enemy")
+          .setDisplaySize(brickWidth, brickHeight);
+        brick.body.setBounce(1);
+        this.vfx.scaleGameObject(brick);
+      }
+    }
+
+    // Set the paddle position:
+    // - For PC devices: y = 600
+    // - For mobile devices: place the paddle at 75% of the game height
+    const paddleY = isMobile ? this.height * 0.75 : 600;
+    this.paddle = this.physics.add
+      .sprite(this.width * 0.5, paddleY, "platform")
+      .setDisplaySize(200, 35)
+      .setImmovable();
+    this.paddle.refreshBody();
+
+    this.ball = this.physics.add
+      .sprite(this.width * 0.5, this.paddle.y - 16, "projectile")
+      .setDisplaySize(48, 48)
+      .setOrigin(0.5)
+      .setCollideWorldBounds(true)
+      .setBounce(1);
+    this.ball.refreshBody();
+    this.ball.setData("onPaddle", true);
+
+    this.physics.add.collider(
+      this.ball,
+      this.paddle,
+      this.ballHitPaddle,
+      null,
+      this
+    );
+    this.physics.add.collider(
+      this.ball,
+      this.bricks,
+      this.ballHitBrick,
+      null,
+      this
+    );
+
+    this.input.on("pointerdown", this.releaseBall, this);
+
+    this.gameOverText = this.add
+      .bitmapText(this.width / 2, 40, "pixelfont", "GAME OVER !", 40)
+      .setDepth(11)
+      .setOrigin(0.5)
+      .setTint(0xff0000)
+      .setAlpha(0);
   }
 
   update(time, delta) {
-    gameSceneUpdate(this, time, delta);
+    // Moved gameSceneUpdate logic into the update method
+    this.paddle.x = this.input.x;
+
+    if (this.paddle.x < 24) {
+      this.paddle.x = 24;
+    } else if (this.paddle.x > this.physics.world.bounds.width - 24) {
+      this.paddle.x = this.physics.world.bounds.width - 24;
+    }
+
+    if (this.ball.getData("onPaddle")) {
+      this.ball.x = this.paddle.x;
+    }
+
+    if (this.ball.body.y > this.height) {
+      this.gameOverText.setAlpha(1);
+      this.sound.stopAll();
+      this.sounds.lose.play();
+      this.physics.pause();
+      this.time.delayedCall(500, () => {
+        this.gameOver();
+      });
+    }
+  }
+
+  // Moved releaseBall function into a class method
+  releaseBall() {
+    if (this.ball.getData("onPaddle")) {
+      this.ball.setVelocity(-75, -300);
+      this.ball.setData("onPaddle", false);
+    }
+  }
+
+  // Moved ballHitBrick function into a class method
+  ballHitBrick(ball, brick) {
+    this.vfx.createEmitter("enemy", brick.x, brick.y, 0.01, 0, 200).explode(30);
+    brick.destroy();
+    this.sounds.destroy.play();
+    this.updateScore(1);
+
+    if (this.bricks.countActive() === 0) {
+      this.sound.stopAll();
+      this.sounds.success.play();
+      this.gameOverText.setAlpha(1).setTint(0x00ff00).setText("YOU WON !");
+      this.physics.pause();
+      this.time.delayedCall(1500, () => {
+        this.gameOver();
+      });
+    }
+  }
+
+  // Moved ballHitPaddle function into a class method
+  ballHitPaddle(ball, paddle) {
+    let diff = 0;
+    this.sounds.jump.play();
+    if (ball.x < paddle.x) {
+      diff = paddle.x - ball.x;
+      ball.setVelocityX(-10 * diff);
+    } else if (ball.x > paddle.x) {
+      diff = ball.x - paddle.x;
+      ball.setVelocityX(10 * diff);
+    } else {
+      ball.setVelocityX(2 + Math.random() * 8);
+    }
   }
 
   updateScore(points) {
@@ -149,137 +294,7 @@ const config = {
   dataObject: {
     name: _CONFIG.title,
     description: _CONFIG.description,
-    instructions: _CONFIG.instructions,
+    instructions32: _CONFIG.instructions,
   },
   orientation: _CONFIG.deviceOrientation === "landscape",
 };
-
-function gameSceneCreate(game) {
-  game.physics.world.setBoundsCollision(1, 1, 1, 0);
-
-  bricks = game.physics.add.group({
-    immovable: true,
-    allowGravity: false,
-  });
-
-  // Set brick formation settings based on device type:
-  // For mobile devices: 13 columns and 5 rows
-  // For PC devices: 15 columns and 4 rows
-  const numBricksX = isMobile ? 13 : 15;
-  const numBricksY = isMobile ? 5 : 4;
-  const brickSpacingX = 50;
-  const brickSpacingY = 52;
-  const brickWidth = 64;
-  const brickHeight = 64;
-
-  // Calculate total width of the brick formation and starting X coordinate to center it
-  const formationWidth = brickSpacingX * (numBricksX - 1) + brickWidth;
-  const startX = (game.width - formationWidth) / 2 + brickWidth / 2;
-  const startY = 100;
-
-  let brick;
-  for (let y = 0; y < numBricksY; y++) {
-    for (let x = 0; x < numBricksX; x++) {
-      const brickX = startX + x * brickSpacingX;
-      const brickY = startY + y * brickSpacingY;
-      brick = bricks
-        .create(brickX, brickY, "enemy")
-        .setDisplaySize(brickWidth, brickHeight);
-      brick.body.setBounce(1);
-      game.vfx.scaleGameObject(brick);
-    }
-  }
-
-  // Set the paddle position:
-  // - For PC devices: y = 600
-  // - For mobile devices: place the paddle at 75% of the game height (i.e. midway in the bottom half)
-  const paddleY = isMobile ? game.height * 0.75 : 600;
-  paddle = game.physics.add
-    .sprite(game.width * 0.5, paddleY, "platform")
-    .setDisplaySize(200, 35)
-    .setImmovable();
-  paddle.refreshBody();
-
-  ball = game.physics.add
-    .sprite(game.width * 0.5, paddle.y - 16, "projectile")
-    .setDisplaySize(48, 48)
-    .setOrigin(0.5)
-    .setCollideWorldBounds(true)
-    .setBounce(1);
-  ball.refreshBody();
-  ball.setData("onPaddle", true);
-
-  game.physics.add.collider(ball, paddle, ballHitPaddle, null, game);
-  game.physics.add.collider(ball, bricks, ballHitBrick, null, game);
-
-  game.input.on("pointerdown", releaseBall, game);
-
-  game.gameOverText = game.add
-    .bitmapText(game.width / 2, 40, "pixelfont", "GAME OVER !", 40)
-    .setDepth(11)
-    .setOrigin(0.5)
-    .setTint(0xff0000)
-    .setAlpha(0);
-}
-
-function gameSceneUpdate(game) {
-  paddle.x = game.input.x;
-
-  if (paddle.x < 24) {
-    paddle.x = 24;
-  } else if (paddle.x > game.physics.world.bounds.width - 24) {
-    paddle.x = game.physics.world.bounds.width - 24;
-  }
-
-  if (ball.getData("onPaddle")) {
-    ball.x = paddle.x;
-  }
-
-  if (ball.body.y > game.height) {
-    game.gameOverText.setAlpha(1);
-    game.sound.stopAll();
-    game.sounds.lose.play();
-    game.physics.pause();
-    game.time.delayedCall(500, () => {
-      game.gameOver();
-    });
-  }
-}
-
-function releaseBall() {
-  if (ball.getData("onPaddle")) {
-    ball.setVelocity(-75, -300);
-    ball.setData("onPaddle", false);
-  }
-}
-
-function ballHitBrick(ball, brick) {
-  this.vfx.createEmitter("enemy", brick.x, brick.y, 0.01, 0, 200).explode(30);
-  brick.destroy();
-  this.sounds.destroy.play();
-  this.updateScore(1);
-
-  if (bricks.countActive() === 0) {
-    this.sound.stopAll();
-    this.sounds.success.play();
-    this.gameOverText.setAlpha(1).setTint(0x00ff00).setText("YOU WON !");
-    this.physics.pause();
-    this.time.delayedCall(1500, () => {
-      this.gameOver();
-    });
-  }
-}
-
-function ballHitPaddle(ball, paddle) {
-  let diff = 0;
-  this.sounds.jump.play();
-  if (ball.x < paddle.x) {
-    diff = paddle.x - ball.x;
-    ball.setVelocityX(-10 * diff);
-  } else if (ball.x > paddle.x) {
-    diff = ball.x - paddle.x;
-    ball.setVelocityX(10 * diff);
-  } else {
-    ball.setVelocityX(2 + Math.random() * 8);
-  }
-}
