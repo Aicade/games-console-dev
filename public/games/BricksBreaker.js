25b800ae6b18d57f2bb8c112a8c2ac7f414dae1d
Nishchal Gupta <nishchalgupta1907@gmail.com>
2025-06-09 01:22:23 +0530

    updated bridgebuilder.json by removing catbox links

--- public/games/BricksBreaker.js
+++ public/games/BricksBreaker.js
@@ -1,10 +1,10 @@
 // Detect mobile devices and override orientation accordingly
-const isMobile = /Mobi|Android/i.test(navigator.userAgent);
-_CONFIG.deviceOrientation = isMobile ? 'portrait' : 'landscape';
+const isMobile = /Mobile|Android/i.test(navigator.userAgent);
+_CONFIG.deviceOrientation = isMobile ? "portrait" : "landscape";
 
 class GameScene extends Phaser.Scene {
   constructor() {
-    super({ key: 'GameScene' });
+    super({ key: "GameScene" });
   }
 
   preload() {
@@ -15,11 +15,13 @@ class GameScene extends Phaser.Scene {
       this.load.audio(key, [_CONFIG.soundsLoader[key]]);
     }
 
-    this.load.image('heart', 'https://aicade-ui-assets.s3.amazonaws.com/GameAssets/icons/heart.png');
-    this.load.bitmapFont('pixelfont',
-      'https://aicade-ui-assets.s3.amazonaws.com/GameAssets/fonts/pix.png',
-      'https://aicade-ui-assets.s3.amazonaws.com/GameAssets/fonts/pix.xml');
-    this.load.image("pauseButton", "https://aicade-ui-assets.s3.amazonaws.com/GameAssets/icons/pause.png");
+    this.load.image("heart", _CONFIG.libLoader.heart);
+    this.load.bitmapFont(
+      "pixelfont",
+      _CONFIG.fontLoader.pixelfont_png,
+      _CONFIG.fontLoader.pixelfont_xml
+    );
+    this.load.image("pauseButton", _CONFIG.libLoader.pauseButton);
 
     addEventListenersPhaser.bind(this)();
     displayProgressLoader.call(this);
@@ -30,18 +32,31 @@ class GameScene extends Phaser.Scene {
     this.width = this.game.config.width;
     this.height = this.game.config.height;
 
-    this.bg = this.add.image(this.game.config.width / 2, this.game.config.height / 2, "background").setOrigin(0.5);
+    this.bg = this.add
+      .image(
+        this.game.config.width / 2,
+        this.game.config.height / 2,
+        "background"
+      )
+      .setOrigin(0.5);
     // Scale the background image to cover the whole canvas
-    const scale = Math.max(this.game.config.width / this.bg.displayWidth, this.game.config.height / this.bg.displayHeight);
+    const scale = Math.max(
+      this.game.config.width / this.bg.displayWidth,
+      this.game.config.height / this.bg.displayHeight
+    );
     this.bg.setScale(scale);
 
-    this.scoreText = this.add.bitmapText(10, 10, 'pixelfont', 'Score: 0', 28);
+    this.scoreText = this.add.bitmapText(10, 10, "pixelfont", "Score: 0", 28);
 
-    this.input.keyboard.on('keydown-ESC', () => this.pauseGame());
-    this.pauseButton = this.add.image(this.game.config.width - 60, 60, "pauseButton");
-    this.pauseButton.setInteractive({ cursor: 'pointer' });
+    this.input.keyboard.on("keydown-ESC", () => this.pauseGame());
+    this.pauseButton = this.add.image(
+      this.game.config.width - 60,
+      60,
+      "pauseButton"
+    );
+    this.pauseButton.setInteractive({ cursor: "pointer" });
     this.pauseButton.setScale(2).setScrollFactor(0).setDepth(11);
-    this.pauseButton.on('pointerdown', () => this.pauseGame());
+    this.pauseButton.on("pointerdown", () => this.pauseGame());
     this.vfx = new VFXLibrary(this);
     gameSceneCreate(this);
 
@@ -80,32 +95,34 @@ class GameScene extends Phaser.Scene {
 function displayProgressLoader() {
   let width = 320;
   let height = 50;
-  let x = (this.game.config.width / 2) - 160;
-  let y = (this.game.config.height / 2) - 50;
+  let x = this.game.config.width / 2 - 160;
+  let y = this.game.config.height / 2 - 50;
 
   const progressBox = this.add.graphics();
   progressBox.fillStyle(0x222222, 0.8);
   progressBox.fillRect(x, y, width, height);
 
-  const loadingText = this.make.text({
-    x: this.game.config.width / 2,
-    y: this.game.config.height / 2 + 20,
-    text: 'Loading...',
-    style: {
-      font: '20px monospace',
-      fill: '#ffffff'
-    }
-  }).setOrigin(0.5, 0.5);
+  const loadingText = this.make
+    .text({
+      x: this.game.config.width / 2,
+      y: this.game.config.height / 2 + 20,
+      text: "Loading...",
+      style: {
+        font: "20px monospace",
+        fill: "#ffffff",
+      },
+    })
+    .setOrigin(0.5, 0.5);
   loadingText.setOrigin(0.5, 0.5);
 
   const progressBar = this.add.graphics();
-  this.load.on('progress', (value) => {
+  this.load.on("progress", (value) => {
     progressBar.clear();
     progressBar.fillStyle(0x364afe, 1);
     progressBar.fillRect(x, y, width * value, height);
   });
-  this.load.on('fileprogress', function (file) {});
-  this.load.on('complete', function () {
+  this.load.on("fileprogress", function (file) {});
+  this.load.on("complete", function () {
     progressBar.destroy();
     progressBox.destroy();
     loadingText.destroy();
@@ -134,7 +151,7 @@ const config = {
     description: _CONFIG.description,
     instructions: _CONFIG.instructions,
   },
-  orientation: _CONFIG.deviceOrientation === "landscape"
+  orientation: _CONFIG.deviceOrientation === "landscape",
 };
 
 function gameSceneCreate(game) {
@@ -142,7 +159,7 @@ function gameSceneCreate(game) {
 
   bricks = game.physics.add.group({
     immovable: true,
-    allowGravity: false
+    allowGravity: false,
   });
 
   // Set brick formation settings based on device type:
@@ -165,7 +182,9 @@ function gameSceneCreate(game) {
     for (let x = 0; x < numBricksX; x++) {
       const brickX = startX + x * brickSpacingX;
       const brickY = startY + y * brickSpacingY;
-      brick = bricks.create(brickX, brickY, 'enemy').setDisplaySize(brickWidth, brickHeight);
+      brick = bricks
+        .create(brickX, brickY, "enemy")
+        .setDisplaySize(brickWidth, brickHeight);
       brick.body.setBounce(1);
       game.vfx.scaleGameObject(brick);
     }
@@ -175,29 +194,32 @@ function gameSceneCreate(game) {
   // - For PC devices: y = 600
   // - For mobile devices: place the paddle at 75% of the game height (i.e. midway in the bottom half)
   const paddleY = isMobile ? game.height * 0.75 : 600;
-  paddle = game.physics.add.sprite(game.width * 0.5, paddleY, 'platform')
-            .setDisplaySize(200, 35)
-            .setImmovable();
+  paddle = game.physics.add
+    .sprite(game.width * 0.5, paddleY, "platform")
+    .setDisplaySize(200, 35)
+    .setImmovable();
   paddle.refreshBody();
 
-  ball = game.physics.add.sprite(game.width * 0.5, paddle.y - 16, 'projectile')
+  ball = game.physics.add
+    .sprite(game.width * 0.5, paddle.y - 16, "projectile")
     .setDisplaySize(48, 48)
     .setOrigin(0.5)
     .setCollideWorldBounds(true)
     .setBounce(1);
   ball.refreshBody();
-  ball.setData('onPaddle', true);
+  ball.setData("onPaddle", true);
 
   game.physics.add.collider(ball, paddle, ballHitPaddle, null, game);
   game.physics.add.collider(ball, bricks, ballHitBrick, null, game);
 
-  game.input.on('pointerdown', releaseBall, game);
+  game.input.on("pointerdown", releaseBall, game);
 
-  game.gameOverText = game.add.bitmapText(game.width / 2, 40, 'pixelfont', 'GAME OVER !', 40)
-                          .setDepth(11)
-                          .setOrigin(0.5)
-                          .setTint(0xff0000)
-                          .setAlpha(0);
+  game.gameOverText = game.add
+    .bitmapText(game.width / 2, 40, "pixelfont", "GAME OVER !", 40)
+    .setDepth(11)
+    .setOrigin(0.5)
+    .setTint(0xff0000)
+    .setAlpha(0);
 }
 
 function gameSceneUpdate(game) {
@@ -209,7 +231,7 @@ function gameSceneUpdate(game) {
     paddle.x = game.physics.world.bounds.width - 24;
   }
 
-  if (ball.getData('onPaddle')) {
+  if (ball.getData("onPaddle")) {
     ball.x = paddle.x;
   }
 
@@ -225,14 +247,14 @@ function gameSceneUpdate(game) {
 }
 
 function releaseBall() {
-  if (ball.getData('onPaddle')) {
+  if (ball.getData("onPaddle")) {
     ball.setVelocity(-75, -300);
-    ball.setData('onPaddle', false);
+    ball.setData("onPaddle", false);
   }
 }
 
 function ballHitBrick(ball, brick) {
-  this.vfx.createEmitter('enemy', brick.x, brick.y, 0.01, 0, 200).explode(30);
+  this.vfx.createEmitter("enemy", brick.x, brick.y, 0.01, 0, 200).explode(30);
   brick.destroy();
   this.sounds.destroy.play();
   this.updateScore(1);
